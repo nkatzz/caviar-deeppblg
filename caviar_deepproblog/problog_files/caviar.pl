6c9e9b4ab93723cfff10df24d1b8e8220df2dd61
nickmanginas <nickmanginas@gmail.com>
2023-06-16 00:26:29 +0300

    update the logic and start training for meeting

--- caviar_deepproblog/problog_files/caviar.pl
+++ caviar_deepproblog/problog_files/caviar.pl
@@ -3,33 +3,52 @@ nn(caviar_net, [Video, P, T], SE, [active, inactive, walking, running]) :: happe
 holdsAt(Video, F, T) :- previous(T1, T), initiatedAt(F, Video, T1).
 holdsAt(Video, F, T) :- previous(T1, T), previous_step(Video, F, T1), \+terminatedAt(F, Video, T1).
 
-initiatedAt(moving(P1, P2), Video, T) :- 
-    happensAt(Video, P1, T, walking),
-    happensAt(Video, P2, T, walking),
-    close(Video, P1, P2, T, 40).
+initiatedAt(meeting(P1, P2), Video, T) :- 
+    happensAt(Video, P1, T, active),
+    happensAt(Video, P2, T, active),
+    is_close(Video, P1, P2, T, 25).
 
-terminatedAt(moving(P1, P2), Video, T) :- 
-    happensAt(Video, p1, T, active), happensAt(Video, p1, T, active).
+initiatedAt(meeting(P1, P2), Video, T) :- 
+    happensAt(Video, P1, T, active),
+    happensAt(Video, P2, T, inactive),
+    is_close(Video, P1, P2, T, 25).
 
-terminatedAt(moving(P1, P2), Video, T) :- 
-    happensAt(Video, P2, T, active), happensAt(Video, P2, T, inactive).
+initiatedAt(meeting(P1, P2), Video, T) :- 
+    happensAt(Video, P1, T, inactive),
+    happensAt(Video, P2, T, active),
+    is_close(Video, P1, P2, T, 25).
 
-terminatedAt(moving(P1, P2), Video, T) :- 
-    happensAt(Video, P2, T, inactive), happensAt(Video, P2, T, active).
+initiatedAt(meeting(P1, P2), Video, T) :- 
+    happensAt(Video, P1, T, inactive),
+    happensAt(Video, P2, T, inactive),
+    is_close(Video, P1, P2, T, 25).
 
+terminatedAt(meeting(P1, P2), Video, T) :- 
+    happensAt(Video, P1, T, running).
 
-% terminatedAt(meeting(P1, P2), Video, T) :- 
-%     happensAt(Video, P2, T, walking).
+terminatedAt(meeting(P1, P2), Video, T) :- 
+    happensAt(Video, P2, T, running).
+
+% terminatedAt(moving(P1, P2), Video, T) :- 
+%     happensAt(Video, P1, T, dissapear).
 % 
-% terminatedAt(meeting(P1, P2), Video, T) :- 
-%     happensAt(Video, P2, T, walking).
+% terminatedAt(moving(P1, P2), Video, T) :- 
+%     happensAt(Video, P2, T, dissapear).
+
+terminatedAt(meeting(P1, P2), Video, T) :- 
+    happensAt(Video, P1, T, walking), far(Video, P1, P2, T, 25).
+
+terminatedAt(meeting(P1, P2), Video, T) :- 
+    happensAt(Video, P2, T, walking), far(Video, P1, P2, T, 25).
+
 
 previous(T1, T) :- 
     T >= 0, 
     T1 is T-1, 
     T1 >= 0.
 
-close(Video, P1, P2, T, D) :- distance(Video, P1, P2, T, D1), D1 =< D. 
+is_close(Video, P1, P2, T, D) :- distance(Video, P1, P2, T, D1), D1 =< D. 
+far(Video, P1, P2, T, D) :- distance(Video, P1, P2, T, D1), D1 > D. 
 
-0.0::previous_step(tensor(train(0)),moving(p1,p2),0).
+0.0::previous_step(tensor(train(0)),meeting(p1,p2),0).
 distance(tensor(train(0)),p1,p2,0,0).
--- caviar_deepproblog/train.py
+++ caviar_deepproblog/train.py
@@ -1,4 +1,3 @@
-from deepproblog.model import Model
 import torch
 from deepproblog.network import Network
 from deepproblog.train import train_model
@@ -10,11 +9,18 @@ from caviar_deepproblog.data.caviar_utils import load_fold
 from caviar_deepproblog.neural.caviar_net import CaviarNet
 from caviar_deepproblog.data.caviar import CaviarVideos, CaviarDataset
 
+import numpy as np
+import random
+
+random.seed(42)
+np.random.seed(42)
+torch.manual_seed(42)
+
 fold_data = load_fold(1)
 
 caviar_net = CaviarNet(4, 5, 128, 1)
 network = Network(caviar_net, "caviar_net")
-network.optimizer = torch.optim.Adam(caviar_net.parameters(), lr=0.0001)
+network.optimizer = torch.optim.Adam(caviar_net.parameters(), lr=0.001)
 
 model = MarkovModel(
     "/home/whatever/programms/caviar-deepproblog/caviar_deepproblog/problog_files/caviar.pl",
@@ -24,11 +30,11 @@ model.set_engine(ExactEngine(model))
 model.add_tensor_source("train", CaviarVideos(fold_data["train"]["videos"]))
 model.add_tensor_source("test", CaviarVideos(fold_data["test"]["videos"]))
 
-train_dataset = CaviarDataset(fold_data["train"]["labels"], complex_event="moving")
-test_dataset = CaviarDataset(fold_data["test"]["labels"], complex_event="moving")
+train_dataset = CaviarDataset(fold_data["train"]["labels"], complex_event="meeting")
+test_dataset = CaviarDataset(fold_data["test"]["labels"], complex_event="meeting")
 loader = DataLoader(train_dataset, 1, False)
 
 
-train_model(model, loader, 1)
+train_model(model, loader, 5, loss_function_name="cross_entropy")
 
 print(get_fact_accuracy(model, test_dataset))
